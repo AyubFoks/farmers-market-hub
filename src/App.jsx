b7ab187ed80bea0539675857dc86293a0dd6ad68
AyubFoks23 <39079638+AyubFoks@users.noreply.github.com>
2025-07-23 12:04:13 +0300

    Malcolm (#2)
    * Initialize branch
    
    * Added React routing to MVP pages
    
    * Change Navbar to Header
    
    * Product form commit
    
    * productlist commit
    
    ---------
    
    Co-authored-by: Malcolm <kagsmalcolm@gmail.com>
    Co-authored-by: Kingbanda <bandadesignske@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
+<<<<<<< HEAD
+import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Navbar from "./components/Header";
+import Home from "./pages/Home";
+import MarketPlace from "./pages/Buyers";
+import FarmersHub from "./pages/Farmers";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+function App() {
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element ={<Home />} />
+                <Route path="/marketplace" element={<MarketPlace />} />
+                <Route path="/farmers-hub" element={<FarmersHub />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+            </Routes>
+        </Router>
+    )
+=======
 import React from 'react';
 import Header from './components/Header';
 import './styles/App.css';
@@ -10,6 +34,8 @@ function App() {
       </div>
     </div>
   );
+>>>>>>> main
 }
 
 export default App;
+// This code sets up a React application with routing using React Router.
