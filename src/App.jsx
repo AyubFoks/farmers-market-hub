de0cfe5991c434b362e5c439361e71ba017616a4
Malcolm <kagsmalcolm@gmail.com>
2025-07-23 10:35:35 +0300

    Added React routing to MVP pages

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import MarketPlace from "./pages/Buyers";
+import FarmersHub from "./pages/Farmers";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+function App() {
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element ={<Home />} />
+                <Route path="/marketplace" element={<MarketPlace />} />
+                <Route path="/farmers-hub" element={<FarmersHub />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+            </Routes>
+        </Router>
+    )
+}
+
+export default App;
+// This code sets up a React application with routing using React Router.
