19d8d5b56391b486174efdddf617dab703fac179
Kingbanda <bandadesignske@gmail.com>
2025-07-23 22:23:03 +0300

    Postind and deleting a product

---
+++ src/pages/Farmers.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import ProductForm from "../components/ProductForm";
+import ProductCard from "../components/ProductCard";
+
+function FarmersHub() {
+  const [products, setProducts] = useState([]);
+
+  function handleAddProduct(newProduct) {
+    setProducts([...products, newProduct]);
+  }
+
+  function handleDeleteProduct(name) {
+    setProducts(products.filter((p) => p.name !== name));
+  }
+
+  return (
+    <div className="farmers-hub">
+      <h2> Post Product</h2>
+      <ProductForm onSubmit={handleAddProduct} />
+
+      <div className="product-grid">
+        {products.map((product, idx) => (
+          <ProductCard
+            key={idx}
+            product={product}
+            onDelete={handleDeleteProduct}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default FarmersHub;
